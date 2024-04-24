19fb27a8866026bf8c6e3bd481c93a0fa0eb2a54
SecretHubkub <161272478+SecretHubkub@users.noreply.github.com>
2024-04-24 19:16:54 +0700

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,28 +1,44 @@
+_G.Webhook_URL = "https://discord.com/api/webhooks/1232594929109434389/HIRL7cCe2d7u1ZnbeilJm74XmDl2chnJxVGFZZSvxeT7etxzf1IpEP2RNYz4QD2R2ehk"
+spawn(function()
+    sendWebhook2()
+end)
+
+
 local HttpRequest = http_request or request or syn.request
+-- สร้างตัวแปรเพื่อเก็บสถานะการส่ง webhook
+local webhookSent = false
 
-local function sendWebhook()
-    HttpService = game:GetService("HttpService")
-    Webhook_URL = _G.Webhook_URL
+-- สร้างฟังก์ชันสำหรับส่ง webhook
+function sendWebhook2()
+    -- ตรวจสอบว่า webhook ยังไม่ได้ส่ง
+    if not webhookSent then
+        HttpService = game:GetService("HttpService")
+        Webhook_URL = _G.Webhook_URL
 
-    local response = HttpRequest({
-        Url = Webhook_URL,
-        Method = "POST",
-        Headers = {
-            ['Content-Type'] = 'application/json'
-        },
-        Body = HttpService:JSONEncode({
-            ["content"] = "",
-            ["embeds"] = {{
-                ["title"] = "Ride a Cart Simulator log😁",
-                ["description"] = "USERNAME : " .. game.Players.LocalPlayer.DisplayName,
-                ["type"] = "rich", -- ปรับเป็น "rich" หรือ "embed" ตามความเหมาะสม
-                ["color"] = tonumber(0xffffff),
-                ["fields"] = {{
-                    ["name"] = "HWID",
-                    ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(), -- เพิ่ม .Value เพื่อเข้าถึงค่า
-                    ["inline"] = true
+        -- ส่ง webhook
+        local response = HttpRequest({
+            Url = Webhook_URL,
+            Method = "POST",
+            Headers = {
+                ['Content-Type'] = 'application/json'
+            },
+            Body = HttpService:JSONEncode({
+                ["content"] = "",
+                ["embeds"] = {{
+                    ["title"] = "Ride a Cart Simulator😁",
+                    ["description"] = "USERNAME : " .. game.Players.LocalPlayer.DisplayName,
+                    ["type"] = "rich", -- ปรับเป็น "rich" หรือ "embed" ตามความเหมาะสม
+                    ["color"] = tonumber(0xffffff),
+                    ["fields"] = {{
+                        ["name"] = "HWID",
+                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(), -- เพิ่ม .Value เพื่อเข้าถึงค่า
+                        ["inline"] = true
+                    }}
                 }}
-            }}
+            })
         })
-    })
+
+        -- ตั้งค่าสถานะ webhook ให้เป็น true เมื่อส่งแล้ว
+        webhookSent = true
+    end
 end
