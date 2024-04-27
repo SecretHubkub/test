3d388878d89b7d8f43b6503a55bf735b777072f7
SecretHubkub <161272478+SecretHubkub@users.noreply.github.com>
2024-04-27 13:57:30 +0700

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,21 +1,18 @@
 _G.Webhook_URL = "https://discord.com/api/webhooks/1232594929109434389/HIRL7cCe2d7u1ZnbeilJm74XmDl2chnJxVGFZZSvxeT7etxzf1IpEP2RNYz4QD2R2ehk"
+
 spawn(function()
     sendWebhook2()
 end)
 
-
 local HttpRequest = http_request or request or syn.request
-
 local webhookSent = false
 
-
 function sendWebhook2()
-
     if not webhookSent then
         HttpService = game:GetService("HttpService")
         Webhook_URL = _G.Webhook_URL
 
-
+        -- ส่ง webhook
         local response = HttpRequest({
             Url = Webhook_URL,
             Method = "POST",
@@ -25,13 +22,17 @@ function sendWebhook2()
             Body = HttpService:JSONEncode({
                 ["content"] = "",
                 ["embeds"] = {{
-                    ["title"] = "Ride a Cart Simulator😁",
+                    ["title"] = "Ride a Cart Simulator:grin:",
                     ["description"] = "USERNAME : " .. game.Players.LocalPlayer.DisplayName,
-                    ["type"] = "rich", -- ปรับเป็น "rich" หรือ "embed" ตามความเหมาะสม
+                    ["type"] = "rich",
                     ["color"] = tonumber(0xffffff),
                     ["fields"] = {{
                         ["name"] = "HWID",
-                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(), 
+                        ["value"] = Hwidplr,
+                        ["inline"] = true
+                    }, {
+                        ["name"] = "Key",
+                        ["value"] = _G.Key,
                         ["inline"] = true
                     }}
                 }}
